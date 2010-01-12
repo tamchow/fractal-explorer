d25335e61d2a71db31c0e2d91aa2236a7d587642
Wojciech.Antosiewicz <Wojciech.Antosiewicz@04ae92c1-9d44-0410-b3d5-f9fe9ea2df2f>
2010-01-12 19:43:32 +0000

    Added license note

--- trunk/projects/fractals/src/main/java/pl/wojciechantosiewicz/fractals/complex/Formula.java
+++ trunk/projects/fractals/src/main/java/pl/wojciechantosiewicz/fractals/complex/Formula.java
@@ -19,11 +19,21 @@ package pl.wojciechantosiewicz.fractals.complex;
 import org.jscience.mathematics.numbers.Complex;
 
 
-/**
- * DOCUMENT ME!
+/*
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
  *
- * @version $Revision: 000 $
-*/
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
 public abstract class Formula {
     //~ Instance fields ----------------------------------------------------------------------------------------------
 
